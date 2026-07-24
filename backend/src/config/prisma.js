921e72ce8d48f501bb173100935617c6f3f60903
Nok SANAI <narsanai001@gmail.com>
2026-07-24 07:31:03 -0700

    Fix prisma adapter using pg pool

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,13 +1,13 @@
 // src/config/prisma.js
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
+const { Pool } = require("pg");
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DIRECT_URL,
+const pool = new Pool({ 
+  connectionString: process.env.DIRECT_URL || process.env.DATABASE_URL 
 });
 
-const prisma = new PrismaClient({
-  adapter,
-});
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
