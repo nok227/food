7036097f505b265b21d1bbcd77bd2ff36933ceae
Nok SANAI <narsanai001@gmail.com>
2026-07-30 09:05:20 -0700

    uoooo

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // 🟢 2. ตั้งค่า Socket.IO ให้เสถียรบน Render
 const io = new Server(server, {
