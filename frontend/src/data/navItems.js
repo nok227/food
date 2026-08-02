0e892d434f5fcd5d7ed607146e1b69af3f55fe0b
Nok SANAI <narsanai001@gmail.com>
2026-08-02 08:25:22 -0700

    itiititi

--- frontend/src/data/navItems.js
+++ frontend/src/data/navItems.js
@@ -1,9 +1,9 @@
 // frontend/src/data/navItems.js
 export const navItems = [
   { path: "/", label: "ໜ້າຫຼັກ", icon: "🍜" },
+  { path: "/master-data", label: "ຈັດການຂໍ້ມູນ", icon: "⚙️" }, // ຈັດການຂໍ້ມູນ (Category, Size, Unit)
   { path: "/orders", label: "ຈັດການອໍເດີ", icon: "🧾" }, // ເມນູທີສອງ
   { path: "/homepage", label: "ບັນທຶກເມນູ", icon: "🍜" },
-  { path: "/master-data", label: "ຈັດການຂໍ້ມູນ", icon: "⚙️" }, // ຈັດການຂໍ້ມູນ (Category, Size, Unit)
   { path: "/about", label: "ລາຍງານຂໍ້ມູນ", icon: "ℹ️" },
   { path: "/contact", label: "ติดต่อเรา", icon: "✉️" },
 ];
